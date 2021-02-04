a5588f916b0582652851303025c21917546b5a03
Gabor Benko <benkogabor@msn.com>
2021-02-04 18:50:50 +0100

    constructor

--- controllers/usercontroller.go
+++ controllers/usercontroller.go
@@ -1,9 +1,20 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
 
-type usercontroller struct{}
+type usercontroller struct {
+	IdPattern *regexp.Regexp
+}
 
 func (uc usercontroller) seveHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from user controller"))
+}
+
+func newusercontroller() *usercontroller {
+	return &usercontroller{
+		IdPattern: regexp.MustCompile(`^/users/(\d+)/?`),
+	}
 }
