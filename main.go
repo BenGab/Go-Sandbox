7c5135430049c5b4a1028e846cff5e4cf6f4125e
Gabor Benko <benkogabor@msn.com>
2021-02-04 13:47:04 +0100

    basic types

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello the Gophers World.")
+	var i int
+	i = 42
+	println(i)
+
+	var pi float32 = 3.14
+	println(pi)
+
+	var ptif = 3.14
+	println(ptif)
+
+	firstName := "Me"
+	println(firstName)
+
+	c := complex(3, 4)
+	println(c)
+
+	im, re := imag(c), real(c)
+
+	println(im, re)
 }
